8bdadfddc63d40c9e5b361652d1e550f60a119eb
Anas <anascodeworks@gmail.com>
2025-08-30 22:05:09 +0530

    Add Frontend, Inngest and Clerk.

---
+++ config/inngest.js
@@ -0,0 +1,69 @@
+import { Inngest } from "inngest";
+import connectDB from "./db.config.js";
+import User from "@/models/user.js";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "quickcart-next" });
+
+// Ingest Function to save user datta to databse
+
+export const syncUserCreation = inngest.createFunction(
+  {
+    id: "sync-user-from-clerk",
+  },
+  {
+    event: "clerk/user.created",
+  },
+
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      imageUrl: image_url,
+    };
+
+    await connectDB();
+    await User.create(userData);
+  }
+);
+
+// Inngest Function to Update user data in the databse;
+export const syncUserUpdation = inngest.createFunction(
+  {
+    id: "update-user-clerk",
+  },
+  {
+    event: "clerk/user.updated",
+  },
+
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      imageUrl: image_url,
+    };
+
+    await connectDB()
+    await User.findByIdAndUpdate(id, userData)
+
+  }
+);
+
+// Inngest Function to delete from user from md
+
+export const syncUserDeletion = inngest.createFunction(
+    {id : "delete-user-with-clerk"},
+    {event : 'clerk/user/deleted'},
+    async ({event})=>{
+            const {id} = event.data
+
+            await connectDB()
+            await User.findByIdAndDelete(id);
+    },
+)
