67602dbd6d0ecce56121bbcf5cbcc5224a9f9b2f
Anas <anascodeworks@gmail.com>
2025-08-30 23:31:17 +0530

    Fix Delete User

--- config/inngest.js
+++ config/inngest.js
@@ -59,7 +59,7 @@ export const syncUserUpdation = inngest.createFunction(
 
 export const syncUserDeletion = inngest.createFunction(
     {id : "delete-user-with-clerk"},
-    {event : 'clerk/user/deleted'},
+    {event : 'clerk/user.deleted'},
     async ({event})=>{
             const {id} = event.data
 
